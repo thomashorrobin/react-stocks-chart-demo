6d2c04bbb8b2ba74e863d518f7c8f7ac07ea390d
Thomas Horrobin <thomas.horrobin@nzx.com>
2017-01-24 17:25:59 +1300

    added NzxChart component

---
+++ src/chart/chart.js
@@ -0,0 +1,13 @@
+import { timeParse } from "d3-time-format";
+import React from "react";
+
+
+class NzxChart extends React.Component {
+    render(){
+        return(
+            <p>hello hello</p>
+        );
+    }
+}
+
+export default NzxChart;
